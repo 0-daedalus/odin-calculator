ddac2ddcd07da46d72e3b0c4aa196bc866fcf1c4
0-daedalus <armageddon_WO@mail.ru>
2022-08-03 19:53:53 +0600

    Complete simple calculating functions

--- scripts.js
+++ scripts.js
@@ -39,19 +39,83 @@ function factorial(x){
 
 function operate(operator, x, y){
     switch(operator){
-        case '+': return add(x,y);
+        case 'add': return add(x,y);
         
-        case '-': return subtract(x,y);
+        case 'subtract': return subtract(x,y);
         
-        case '*': return multiply(x,y);
+        case 'multiply': return multiply(x,y);
         
-        case '/': return divide(x,y);
+        case 'divide': return divide(x,y);
         
-        case '^': return pow(x,y);
+        case 'power': return pow(x,y);
         
         case 'sqrt': return sqrt(x);
         
-        case '!': return factorial(x);
+        case 'factorial': return factorial(x);
         
     }
 }
+
+//////////////////////////////////////////////////////
+let display = document.querySelector('.display p');
+let displayValue = '';
+let isDecimal = false;
+
+function clear(){
+    displayValue = '';
+    isDecimal = false;
+    display.textContent = displayValue;
+}
+
+function update(){
+    display.textContent = displayValue;
+}
+
+let num1 = null;
+let num2 = null;
+let operator = '';
+let operationFinished = false;
+
+let numbers = document.querySelectorAll('.number-key');
+
+numbers.forEach((key) => {
+    key.addEventListener('click', () => {
+        if(key.id === 'decimal') {
+            if(!isDecimal){
+                displayValue += '.';
+                isDecimal = true;
+            }
+        }
+        else if(key.id === 'clear'){
+            clear();
+        }
+        else displayValue += key.id;
+        update();
+    });
+});
+
+let operators = document.querySelectorAll('.operator');
+
+operators.forEach((key) => {
+    key.addEventListener('click', () => {
+        operationFinished = false;
+        num1 = +displayValue;
+        clear();
+        operator = key.id;
+    });
+});
+
+let enter = document.querySelector('.enter-key');
+
+enter.addEventListener('click', () => {
+    if(!operationFinished){
+        num2 = +displayValue;
+        if(num1){
+            displayValue = operate(operator, num1, num2);
+            update();
+            operationFinished = true;
+            num1 = null;
+            num2 = null;
+        }
+}
+})
